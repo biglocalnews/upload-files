a2a75b0ae0a6e3f35a1bdf873188eaae458f9e34
palewire <b@palewi.re>
2022-02-01 06:27:50 -0800

    Dur

--- upload.py
+++ upload.py
@@ -18,13 +18,17 @@ def main():
     if path.endswith("/"):
         # Upload directory
         file_list = glob.glob(path + "*.csv")
+        if not file_list:
+            raise ValueError("No CSV file found in directory.")
         for f in file_list:
             print(f"Uploading {f}")
         client.upload_files(project_id, file_list)
     else:
         # Upload file
+        if not path.lower().endswith("csv"):
+            raise ValueError("Files must be CSVs")
         print(f"Uploading {path}")
-        client.upload_file(project_id, files)
+        client.upload_file(project_id, path)
 
 
 if __name__ == '__main__':
