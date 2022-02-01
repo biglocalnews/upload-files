bc461e9e3a5422887b187d44ad866b1e646e82ee
palewire <b@palewi.re>
2022-02-01 06:26:41 -0800

    Directory thing

--- upload.py
+++ upload.py
@@ -1,3 +1,4 @@
+import glob
 import os
 
 from bln.client import Client
@@ -10,15 +11,19 @@ def main():
     api_key = os.getenv("api-key")
     project_id = os.getenv("project-id")
     path = os.getenv("path")
-    print(f"Uploading {path}")
 
     # Upload files
     client = Client(api_key)
 
     if path.endswith("/"):
         # Upload directory
+        file_list = glob.glob(path + "*.csv")
+        for f in file_list:
+            print(f"Uploading {f}")
+        client.upload_files(project_id, file_list)
     else:
         # Upload file
+        print(f"Uploading {path}")
         client.upload_file(project_id, files)
 
 
