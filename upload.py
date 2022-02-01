55f6777c6a4ddfc236207c1aaa2582f1da9a61d4
palewire <b@palewi.re>
2022-02-01 06:46:36 -0800

    Tidy

--- upload.py
+++ upload.py
@@ -15,22 +15,36 @@ def main():
     # Upload files
     client = Client(api_key)
 
+    # If the path is a directory ...
     if path.endswith("/"):
-        # Upload directory
+        # Get all the CSV files in the directory
         file_list = glob.glob(path + "*.csv")
+
+        # Make sure we have at least one
         if not file_list:
             raise ValueError("No CSV file found in directory.")
+
+        # Upload the files
         for f in file_list:
             print(f"Uploading {f}")
-        client.upload_files(project_id, file_list)
+            client.upload_file(project_id, f)
+
+    # If a single file ...
     else:
-        # Upload file
+        # Make sure it's a CSV
         if not path.lower().endswith("csv"):
             raise ValueError("Files must be CSVs")
-        if not path.startswith("/") and not path.startswith("."):
-            path = os.getcwd() + path
+
+        # If it's not an absolute path ..
+        if not path.startswith("/"):
+            # ... tack on the working directory
+            path = os.path.join(os.getcwd() + "/", path)
+
+        # Make sure the path exists
         if not os.path.exists(path):
             raise ValueError(f"File path does not exist {path}")
+
+        # Upload the file
         print(f"Uploading {path}")
         client.upload_file(project_id, path)
 
